f5a7103cd958542f0d4b9bad70fd0263ea63c4df
anand ramnayan maurya <anandmr05@gmail.com>
2020-10-12 02:42:09 +0530

    Updates

---
+++ precache-manifest.01c5ef3bf2be1d4a657c68e97d84b580.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a2f0d603657bb58a240ff6634b0986",
+    "url": "/MERN_PR/index.html"
+  },
+  {
+    "revision": "f2b937d29983358664f3",
+    "url": "/MERN_PR/static/css/2.b207269f.chunk.css"
+  },
+  {
+    "revision": "2a5673453e3c94fe7946",
+    "url": "/MERN_PR/static/css/main.7618b784.chunk.css"
+  },
+  {
+    "revision": "f2b937d29983358664f3",
+    "url": "/MERN_PR/static/js/2.088c93bf.chunk.js"
+  },
+  {
+    "revision": "863e5dfaa7783b08dc1d03326e06085a",
+    "url": "/MERN_PR/static/js/2.088c93bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a5673453e3c94fe7946",
+    "url": "/MERN_PR/static/js/main.07dca3d2.chunk.js"
+  },
+  {
+    "revision": "1edd09c9d52ae9f99412",
+    "url": "/MERN_PR/static/js/runtime-main.ba6d2c4f.js"
+  },
+  {
+    "revision": "79d2ebcb2b879eb167b11d452b49566e",
+    "url": "/MERN_PR/static/media/anand.79d2ebcb.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MERN_PR/static/media/logo.5d5d9eef.svg"
+  }
+]);
